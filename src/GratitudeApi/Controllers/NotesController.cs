2d2e228095e8c5c102d126ee30a86a3bbd0efa2c
dejital <sergey@sergeyk.com>
2015-11-12 17:39:16 -0600

    Implement GET methods in NotesController

---
+++ src/GratitudeApi/Controllers/NotesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using GratitudeApi.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace GratitudeApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotesController : Controller
+    {
+        [FromServices]
+        public INoteRepository Notes { get; set; }
+
+        [HttpGet]
+        public IEnumerable<Note> GetAll()
+        {
+            return Notes.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetNote")]
+        public IActionResult GetById(string id)
+        {
+            var note = Notes.Find(id);
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+            return new ObjectResult(note);
+        }
+    }
+}
--- src/GratitudeApi/Startup.cs
+++ src/GratitudeApi/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GratitudeApi.Models;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
@@ -13,6 +10,8 @@ namespace GratitudeApi
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<INoteRepository, NoteRepository>();
         }
 
         public void Configure(IApplicationBuilder app)
